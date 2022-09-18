8e4ea4b3bafdf121e357554b17bbd1a154f7c1fd
StudioPaap <anoukpaap@gmail.com>
2022-09-18 18:02:01 +0200

    Push 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main (String[] args){
+        int[] numeric = { 1,2,3,4,5,6,7,8,9,0};
+        String[] alfabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen"};
+
+    }
+}
---
+++ src/Translator.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Translator {
+    Map<Integer,String>numericAlpha = new HashMap<>();
+
+//    for(int i = 0; i < numeric.length; i++) {
+//
+//    };
+//
+//     for(int i = 0; i < alfabetic.length; i++) {
+//
+    };
+
+}
